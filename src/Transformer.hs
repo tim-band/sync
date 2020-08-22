af67ce64f2bf520bc8ea1ec304d39a672bf1462d
Tim Band <t.band>
2020-08-22 22:02:15 +0100

    Fixed basic case

--- src/Transformer.hs
+++ src/Transformer.hs
@@ -4,11 +4,14 @@ module Transformer
     ( parseValue
     ) where
 
-import Control.Applicative ((<$>), (<|>))
+import Control.Applicative ((<$>))
 import Control.Monad (sequence)
-import Data.Yaml (Object, Array, Value, Parser, decodeFileThrow, withArray, withObject, (.:))
+import Data.Aeson.Types (parseFail, prependFailure)
+import Data.Monoid ((<>))
+import Data.Yaml (Object, Array, Value(..), Parser, decodeFileThrow, withArray, withObject, (.:))
 import qualified Path
 import qualified Copy
+import qualified Drives
 import TransformerParser (PathFinder, PathFinderO, PathFinderV, chain)
 import Data.List (foldl')
 import qualified Data.Vector as Vec
@@ -17,23 +20,28 @@ tags :: [(String, PathFinderO)]
 tags =
     [ (Copy.name, Copy.parser)
     , (Path.name, Path.parser)
+    , (Drives.name, Drives.parser)
     ]
 
 parseObject :: PathFinderO
 parseObject tr ob = do
     tag <- ob .: "t"
     case (lookup tag tags) of
-        Nothing -> fail ("Unknown tag " ++ tag)
-        Just f -> f tr ob
+        Nothing -> parseFail ("Unknown tag " ++ tag)
+        Just f -> prependFailure ("(" ++ tag ++ ")") $ f tr ob
 
 parseArray :: PathFinderV -> Array -> Parser PathFinder
 parseArray tr ar = let
     pfs :: Parser [PathFinder]
-    pfs = sequence $ Vec.toList $ Vec.map tr ar
+    pfs = sequence
+        $ zipWith (\i a -> prependFailure ("[" ++ show i ++ "]") (tr a)) [1..]
+        $ Vec.toList ar
     runAll :: [PathFinder] -> FilePath -> IO [FilePath]
     runAll fs p = foldl' chain (return [p]) fs
     in runAll <$> pfs
 
 parseValue :: PathFinderV
-parseValue v = withArray "array" (parseArray parseValue) v
-    <|>  withObject "object" (parseObject parseValue) v
+parseValue v = case v of
+    Array ar -> parseArray parseValue ar
+    Object ob -> parseObject parseValue ob
+    _ -> parseFail ("Expected array or object")
